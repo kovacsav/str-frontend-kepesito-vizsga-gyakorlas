869b4484ad0898c02b2af5319d675414db2a31a3
kovacsav <mezbogyo@gmail.com>
2023-01-17 18:47:31 +0100

    end

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,19 +1,36 @@
 function changeOuterLinks() {
-    const links = document.querySelectorAll('nav a');
-    links.forEach(link => {
-        if (link.innerHTML.includes('outer-link')) {
-            link.target = '_blank';
-            link.innerHTML = `<strong>${link.innerHTML}</strong>`;
-        }
-    });
-    const nav = document.querySelector('nav');
-    nav.style.display = 'flex';
-    nav.style.flexDirection = 'column';
-    nav.style.width = '30%';
-    nav.style.margin = '0 auto';
-    nav.style.border = '1px solid blue';
-    nav.style.padding = '16px';
+  const navLinks = document.querySelectorAll("nav a");
+  navLinks.forEach((item) => {
+    if (item.innerHTML.includes("outer-link")) {
+      item.target = "_blank";
+      item.innerHTML = `<strong>${item.innerHTML}</strong>`;
+    }
+  });
+  const nav = document.querySelector("nav");
+  nav.style.display = "flex";
+  nav.style.flexDirection = "column";
+  nav.style.width = "30%";
+  nav.style.margin = "0 auto";
+  nav.style.border = "solid blue 1px";
+  nav.style.padding = "16px";
 }
-changeOuterLinks()
+changeOuterLinks();
 
 export { changeOuterLinks };
+
+/*
+const links = document.querySelectorAll("nav a");
+links.forEach((link) => {
+  if (link.innerHTML.includes("outer-link")) {
+    link.target = "_blank";
+    link.innerHTML = `<strong>${link.innerHTML}</strong>`;
+  }
+});
+const nav = document.querySelector("nav");
+nav.style.display = "flex";
+nav.style.flexDirection = "column";
+nav.style.width = "30%";
+nav.style.margin = "0 auto";
+nav.style.border = "1px solid blue";
+nav.style.padding = "16px";
+*/
